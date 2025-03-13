3691923d56c96d24de7f7b0730997b195fb2b38a
FruitPunchSamurai <sheshan1961@gmail.com>
2025-03-13 19:11:50 -0400

    refactoring and add the generated Expr.h file from generateAST

---
+++ include/Expr.h
@@ -0,0 +1,85 @@
+#pragma once
+#include <memory>
+#include <variant>
+#include "../include/Token.h"
+namespace lox {
+class Binary;
+class Grouping;
+class Literal;
+class Unary;
+class Visitor {
+public:
+virtual ~Visitor() = default;
+virtual void visitBinaryExpr(const Binary& expr) = 0;
+virtual void visitGroupingExpr(const Grouping& expr) = 0;
+virtual void visitLiteralExpr(const Literal& expr) = 0;
+virtual void visitUnaryExpr(const Unary& expr) = 0;
+};
+class Expr {
+public:
+virtual ~Expr() = default;
+virtual void accept(Visitor& visitor) const = 0;
+};
+class Binary final : public Expr {
+    std::shared_ptr<Expr> left;
+    Token op;
+    std::shared_ptr<Expr> right;
+
+public:
+    Binary(std::shared_ptr<Expr> left, Token op, std::shared_ptr<Expr> right)
+        : left(std::move(left)), op(std::move(op)), right(std::move(right)) {}
+
+    void accept(Visitor& visitor) const override {
+        visitor.visitBinaryExpr(*this);
+    }
+
+[[nodiscard]] const std::shared_ptr<Expr>& getLeft() const { return left; }
+[[nodiscard]] const Token& getOp() const { return op; }
+[[nodiscard]] const std::shared_ptr<Expr>& getRight() const { return right; }
+};
+
+class Grouping final : public Expr {
+    std::shared_ptr<Expr> expression;
+
+public:
+    explicit Grouping(std::shared_ptr<Expr> expression)
+        : expression(std::move(expression)) {}
+
+    void accept(Visitor& visitor) const override {
+        visitor.visitGroupingExpr(*this);
+    }
+
+[[nodiscard]] const std::shared_ptr<Expr>& getExpression() const { return expression; }
+};
+
+class Literal final : public Expr {
+    std::variant<double, std::string, bool, std::nullptr_t> value;
+
+public:
+    explicit Literal(std::variant<double, std::string, bool, std::nullptr_t> value)
+        : value(std::move(value)) {}
+
+    void accept(Visitor& visitor) const override {
+        visitor.visitLiteralExpr(*this);
+    }
+
+[[nodiscard]] const std::variant<double, std::string, bool, std::nullptr_t>& getValue() const { return value; }
+};
+
+class Unary final : public Expr {
+    Token op;
+    std::shared_ptr<Expr> right;
+
+public:
+    Unary(Token op, std::shared_ptr<Expr> right)
+        : op(std::move(op)), right(std::move(right)) {}
+
+    void accept(Visitor& visitor) const override {
+        visitor.visitUnaryExpr(*this);
+    }
+
+[[nodiscard]] const Token& getOp() const { return op; }
+[[nodiscard]] const std::shared_ptr<Expr>& getRight() const { return right; }
+};
+
+}
