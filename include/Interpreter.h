a0c4c3168c0beca2d20805d72f0016389ca3e011
FruitPunchSamurai <sheshan1961@gmail.com>
2025-04-16 22:24:33 -0400

    finish visit binary method

--- include/Interpreter.h
+++ include/Interpreter.h
@@ -5,18 +5,22 @@
 
 #include "Expr.h"
 
-namespace lox
-{
-    class Interpreter : public Visitor
-    {
+namespace lox {
+    class Interpreter : public Visitor {
     public:
-        TokenLiteral visitGroupingExpr(const Grouping& expr) override;
-        TokenLiteral visitLiteralExpr(const Literal& expr) override;
-        TokenLiteral visitUnaryExpr(const Unary& expr) override;
+        TokenLiteral visitGroupingExpr(const Grouping &expr) override;
+
+        TokenLiteral visitLiteralExpr(const Literal &expr) override;
+
+        TokenLiteral visitUnaryExpr(const Unary &expr) override;
+
         TokenLiteral visitBinaryExpr(const Binary &expr) override;
 
     private:
         [[nodiscard]] bool isTruthy(TokenLiteral token_literal) const;
-        TokenLiteral evaluate(const std::shared_ptr<Expr>& expr);
+
+        [[nodiscard]] bool isEqual(const TokenLiteral &a, const TokenLiteral &b) const;
+
+        TokenLiteral evaluate(const std::shared_ptr<Expr> &expr);
     };
 }
--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -31,13 +31,24 @@ namespace lox {
         const TokenLiteral right = evaluate(expr.getRight());
 
         switch (expr.getOp().getTokenType()) {
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL:
+                return isEqual(left, right);
+            case GREATER:
+                return std::get<double>(left) > std::get<double>(right);
+            case GREATER_EQUAL:
+                return std::get<double>(left) >= std::get<double>(right);
+            case LESS:
+                return std::get<double>(left) < std::get<double>(right);
+            case LESS_EQUAL:
+                return std::get<double>(left) <= std::get<double>(right);
             case MINUS:
                 return std::get<double>(left) - std::get<double>(right);
             case PLUS:
                 if (std::holds_alternative<double>(left) && std::holds_alternative<double>(right)) {
                     return std::get<double>(left) - std::get<double>(right);
                 }
-
                 if (std::holds_alternative<std::string>(left) && std::holds_alternative<std::string>(right)) {
                     return std::get<std::string>(left) + std::get<std::string>(right);
                 }
@@ -56,6 +67,23 @@ namespace lox {
 
     bool Interpreter::isTruthy(TokenLiteral token_literal) const {
         return std::visit(TokenLiteralToBoolean{}, token_literal);
+    }
+
+    bool Interpreter::isEqual(const TokenLiteral &a, const TokenLiteral &b) const {
+        if (std::holds_alternative<std::monostate>(a) && std::holds_alternative<std::monostate>(b)) return true;
+
+        if (std::holds_alternative<std::monostate>(a) || std::holds_alternative<std::monostate>(b)) return false;
+
+        if (std::holds_alternative<std::string>(a) && std::holds_alternative<std::string>(b))
+            return std::get<std::string>(a) == std::get<std::string>(b);
+
+        if (std::holds_alternative<double>(a) && std::holds_alternative<double>(b))
+            return std::get<double>(a) == std::get<double>(b);
+
+        if (std::holds_alternative<bool>(a) && std::holds_alternative<bool>(b))
+            return std::get<bool>(a) == std::get<bool>(b);
+
+        return false;
     }
 
 
