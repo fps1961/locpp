f533c75996145af3b161d8cef361d3afdf85a82b
FruitPunchSamurai <sheshan1961@gmail.com>
2025-06-11 21:06:24 -0500

    Add initial scopes and implement Block Statement code

--- include/Resolver.h
+++ include/Resolver.h
@@ -3,15 +3,30 @@
 //
 
 #pragma once
+#include <unordered_map>
+
 #include "Expr.h"
 #include "Stmt.h"
 
 namespace lox {
     class Resolver : public StmtVisitor, ExprVisitor {
         Interpreter interpreter;
+        std::vector<std::unordered_map<std::string, bool> > scopes;
 
     public:
         Resolver(Interpreter &interpreter) : interpreter(interpreter) {
         };
+
+        TokenLiteral visitBlockStmt(const Block &stmt) override;
+
+    private:
+        void resolve(const std::vector<std::shared_ptr<Stmt> > &stmts);
+
+        void resolve(const std::shared_ptr<Stmt> &stmt);
+
+        void resolve(const std::shared_ptr<Expr> &expr);
+
+        void beginScope() { scopes.push_back(std::unordered_map<std::string, bool>{}); };
+        void endScope() { scopes.pop_back(); };
     };
 }
---
+++ src/Resolver.cpp
@@ -0,0 +1,30 @@
+//
+// Created by shesh on 6/11/2025.
+//
+
+#include "../include/Resolver.h"
+
+#include <unordered_map>
+
+namespace lox {
+    TokenLiteral Resolver::visitBlockStmt(const Block &stmt) {
+        beginScope();
+        resolve(stmt.getStatements());
+        endScope();
+        return {};
+    }
+
+    void Resolver::resolve(const std::vector<std::shared_ptr<Stmt> > &stmts) {
+        for (const auto &stmt: stmts) {
+            resolve(stmt);
+        }
+    }
+
+    void Resolver::resolve(const std::shared_ptr<Stmt> &stmt) {
+        stmt->accept(*this);
+    }
+
+    void Resolver::resolve(const std::shared_ptr<Expr> &expr) {
+        expr->accept(*this);
+    }
+};
